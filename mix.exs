b08dcc54c2a56f92290d87ca513318434e7879aa
Doc Ritezel <pair+doc@ministryofvelocity.com>
2023-07-25 23:17:56 -0700

    Add package information

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule Zenic.MixProject do
   def project do
     [
       app: :zenic,
+      name: "Zenic",
+      source_url: "https://github.com/ohrite/zenic",
       version: "0.1.0",
       elixir: "~> 1.9",
       build_embedded: true,
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
       deps_path: "../deps.nosync",
@@ -34,4 +37,16 @@ defmodule Zenic.MixProject do
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+
+  defp package() do
+    [
+      name: "zenic",
+      description: "A flat, round, Zdog-inpsired 3D engine for Scenic",
+      contributors: ["Doc Ritezel"],
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/ohrite/zenic"}
+    ]
+  end
 end
